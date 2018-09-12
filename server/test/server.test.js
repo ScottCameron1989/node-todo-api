f3bf70e08b2d46dbb93276eb96874f834fb0277b
ScottCameron1989 <scottcameron1989@gmail.com>
2018-09-12 19:50:51 -0400

    add test for delete /users/me/token

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -247,7 +247,7 @@ describe('POST /users/login', () => {
       }).catch((e) => done(e));
     });
   });
-  
+
   it ('should reject invalid login', (done) => {
     var email = users[1].email;
     var password = 'pas';
@@ -268,5 +268,22 @@ describe('POST /users/login', () => {
           done();
         }).catch((e) => done(e));
       });
+  });
+});
+
+describe('DELETE /users/me/token', ()=> {
+  it('should remove auth token on logout', (done)=>{
+    request(app)
+    .delete('/users/me/token').set('x-auth', users[0].tokens[0].token)
+    .expect(200)
+    .end((err, res) => {
+      if (err){
+        return done(err);
+      }
+      User.findById(users[0]._id).then((user) => {
+        expect(user.tokens.length).toBe(0);
+        done();
+      }).catch((e) => done(e));
+    });
   });
 });
